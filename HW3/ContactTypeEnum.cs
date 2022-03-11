d0d5734e1e7cb69545a4268da6356a30f34e78a7
SadraDN <daviransadra@gmail.com>
2022-03-11 13:11:50 +0330

    HW3

---
+++ HW3/ContactTypeEnum.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+public enum ContactTypeEnum
+{
+   [Display(Name = "پست الکترونیک")]
+    Email = 1,
+    [Display(Name = "پیامک")]
+    Sms = 2
+
+}
+
+
+
+
+
+
---
+++ HW3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+ Enum contactType = ContactTypeEnum.Email;
+Console.WriteLine(" '{0}'",contactType.ToString());
+
+
+NumOfContactType(ContactTypeEnum.Email);
+NumOfContactType(ContactTypeEnum.Sms);
+
+static int NumOfContactType(ContactTypeEnum text)
+{
+    return (int)(text);
+}
+
+
+string str = "Red";
+
+ContactTypeEnum color = (ContactTypeEnum)Enum.Parse(typeof(ContactTypeEnum), str);
+
+
+
+
+
+public string switchcaseEnum((int)
+{
+
+}

ed9c428d45a455973fbfb6330ca9594179292195
SadraDN <daviransadra@gmail.com>
2022-03-17 23:50:54 +0330

    HW4
    takalif hafte 4om

---
+++ HW4/Person.cs
@@ -0,0 +1,14 @@
+﻿public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string IdNumber { get; set; }
+    public int Height { get; set; }
+    public string Mobile { get; set; }
+    public string FatherName { get; set; }
+    public int Weight { get; set; }
+    public string BirthDate { get; set; }
+    public string Address { get; set; }
+
+
+}
---
+++ HW4/Program.cs
@@ -0,0 +1,42 @@
+﻿
+Person User1 = new Person();
+Console.WriteLine("Enter your first name");
+User1.FirstName = Console.ReadLine();
+
+Console.WriteLine("Enter your last name");
+User1.LastName = Console.ReadLine();
+
+Console.WriteLine("Enter your Id card number");
+User1.IdNumber = Console.ReadLine();
+
+Console.WriteLine("Enter your Height");
+User1.Height = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter your Mobile number");
+User1.Mobile = Console.ReadLine();
+
+Console.WriteLine("Enter your father name");
+User1.FatherName = Console.ReadLine();
+
+Console.WriteLine("Enter your weight");
+User1.Weight = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter your birth date");
+User1.BirthDate = Console.ReadLine();
+
+Console.WriteLine("Enter your address");
+User1.Address = Console.ReadLine();
+
+List<Person> list = new List<Person>();
+list.Add(User1);
+
+var path = "d://Maktab//Information.txt";
+File.Create(path);
+
+
+
+string info = User1.FirstName + "," + User1.LastName + "," + User1.IdNumber + "," + User1.Height + "," + User1.Mobile + "," + User1.FatherName + "," + User1.Weight + "," + User1.BirthDate + "," + User1.Address;
+string showinfo = User1.FirstName + "," + User1.LastName + "," + User1.IdNumber;
+
+
+

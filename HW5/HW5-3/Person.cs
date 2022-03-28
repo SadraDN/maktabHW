45e39887cf560ee434308e98e24bf5f223c064a5
SadraDN <daviransadra@gmail.com>
2022-03-28 22:45:18 +0330

    HW5
    takalif dore 5

---
+++ HW5/HW5-3/Person.cs
@@ -0,0 +1,10 @@
+﻿public class Person
+{
+
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Address { get; set; }
+    public int IdCardNum { get; set; }
+    public int Id { get; set; }
+}
---
+++ HW5/HW5/Program.cs
@@ -0,0 +1,44 @@
+﻿var dest = @"C:\\Users\\Sadra\\Desktop\\maktab\\codes\\HW5\\Archive\\CopyDest.txt";
+var source = @"C:\\Users\\Sadra\\Desktop\\maktab\\codes\\HW5\\CopySource.txt";
+
+try
+{
+    File.Copy(source, dest , true);
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine("File doesn't exist");
+}
+catch (UnauthorizedAccessException ex)
+{
+    Console.WriteLine("You Do not have access to this file");
+}
+catch (DirectoryNotFoundException ex)
+{
+    Console.WriteLine("Can't find your destination folder");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Something went wrong , try again");
+}
+
+
+
+
+
+
+
+
+
+
+
+//catch (IOException iox)
+//{
+//    Console.WriteLine(iox.Message);
+//    throw iox;
+//}
+//catch (UnauthorizedAccessException iox)
+//{
+//    Console.WriteLine("Do not have access to File");
+//    throw iox;
+//}
